a75a6f7d9e7a2fb1fcd3c7d9b78dba11af2dca3a
julio-svg <julioangel.hernandez@gmail.com>
2023-03-20 23:52:27 +0100

    forgot file

--- src/main/java/com/comerce/comerce/services/impl/ProductServiceImpl.java
+++ src/main/java/com/comerce/comerce/services/impl/ProductServiceImpl.java
@@ -10,15 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
-import java.util.function.IntPredicate;
-import java.util.function.IntUnaryOperator;
-import java.util.function.Predicate;
 import java.util.function.UnaryOperator;
-import java.util.stream.Collectors;
 
 import static java.time.format.DateTimeFormatter.ISO_LOCAL_DATE_TIME;
 
@@ -28,7 +21,7 @@ import static java.time.format.DateTimeFormatter.ISO_LOCAL_DATE_TIME;
 public class ProductServiceImpl implements ProductService {
 
 
-   UnaryOperator<List<ProductServiceResponseDTO>> getOnlyTheSeasonWithBiggestPriority = productServiceResponseDTOList -> productServiceResponseDTOList.stream()
+   UnaryOperator<List<ProductServiceResponseDTO>> getOnlyTheRegisterWithBiggestPriority = productServiceResponseDTOList -> productServiceResponseDTOList.stream()
            .max((a , b) -> {
                if(a.getPriorityPriceApplication() >= b.getPriorityPriceApplication()){
                    return 1;
@@ -56,7 +49,7 @@ public class ProductServiceImpl implements ProductService {
         List<ProductServiceResponseDTO> productServiceResponseDTOList =  Mapper.fromPricesDTOListToProductServiceResponseDTOList(
                 productRepository.findByproductIdAndCorporateIdAndEndDateIsAfterAndStartDateIsBefore(Integer.valueOf(productId), Integer.valueOf(corporateId), endDate, startDate));
 
-        return getOnlyTheSeasonWithBiggestPriority.apply(productServiceResponseDTOList);
+        return getOnlyTheRegisterWithBiggestPriority.apply(productServiceResponseDTOList);
 
     }
 }
